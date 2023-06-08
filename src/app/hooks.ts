6123bef605eacd36da02ab66a4e8ebadd30b6dfb
Derek Wan <d.wan@icloud.com>
2023-06-08 21:13:38 +0800

    Finish chapter 1 (MERN stack project)

--- src/app/hooks.ts
+++
@@ -1,5 +0,0 @@
-import { TypedUseSelectorHook, useDispatch, useSelector } from "react-redux";
-import type { Dispatch, State } from "./store";
-
-export const useAppSelector: TypedUseSelectorHook<State> = useSelector;
-export const useAppDispatch = () => useDispatch<Dispatch>();
