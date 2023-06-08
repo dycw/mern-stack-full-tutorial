067568f30f99d8b86feaef042f1e955396a4aa03
Derek Wan <d.wan@icloud.com>
2023-06-08 22:57:23 +0800

    Finish chapter 3 (MongoDB) (#4)

---
+++ models/User.js
@@ -0,0 +1,24 @@
+import mongoose, { model } from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  roles: [
+    {
+      type: String,
+      default: "Employee",
+    },
+  ],
+  active: {
+    type: Boolean,
+    default: true,
+  },
+});
+
+export default model("User", userSchema);
