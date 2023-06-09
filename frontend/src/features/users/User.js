71792022807245a4bc1cd7dbe1c60d09cfc8a8d6
Derek Wan <d.wan@icloud.com>
2023-06-09 14:03:17 +0800

    Finish lesson 6 (Redux & RTK query) (#8)

---
+++ frontend/src/features/users/User.js
@@ -0,0 +1,33 @@
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
+
+import { useSelector } from "react-redux";
+import { selectUserById } from "./usersApiSlice";
+
+export default function User({ userId }) {
+  const navigate = useNavigate();
+  const user = useSelector((state) => selectUserById(state, userId));
+
+  if (user) {
+    const handleEdit = () => navigate(`/dash/users/${userId}`);
+
+    const userRolesString = user.roles.toString().replaceAll(",", ", ");
+
+    const cellStatus = user.active ? "" : "table__cell--inactive";
+
+    return (
+      <tr className="table__row user">
+        <td className={`table__cell ${cellStatus}`}>{user.username}</td>
+        <td className={`table__cell ${cellStatus}`}>{userRolesString}</td>
+        <td className={`table__cell ${cellStatus}`}>
+          <button className="icon-button table__button" onClick={handleEdit}>
+            <FontAwesomeIcon icon={faPenToSquare} />
+          </button>
+        </td>
+      </tr>
+    );
+  } else {
+    return null;
+  }
+}
