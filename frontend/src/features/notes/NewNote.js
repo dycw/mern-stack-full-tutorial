856ca75b605afd5ebe7704288e5201170afdbacc
Derek Wan <d.wan@icloud.com>
2023-06-10 16:21:35 +0800

    Finish lesson 9 (login auth)

--- frontend/src/features/notes/NewNote.js
+++ frontend/src/features/notes/NewNote.js
@@ -1,8 +1,12 @@
 import { useSelector } from "react-redux";
+
 import { selectAllUsers } from "../users/usersApiSlice";
 import NewNoteForm from "./NewNoteForm";
 
 export default function NewNote() {
   const users = useSelector(selectAllUsers);
+  if (!users?.length) {
+    return <p>Not Currently Available</p>;
+  }
   return users ? <NewNoteForm users={users} /> : <p>Loading...</p>;
 }
